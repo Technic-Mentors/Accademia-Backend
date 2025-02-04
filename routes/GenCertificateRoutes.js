daf52cafdbcf1d13495eec9d791ef3aaee71f37e
Ata00422 <167747030+Ata00422@users.noreply.github.com>
2025-02-04 11:59:45 +0500

    3 jan push

--- routes/GenCertificateRoutes.js
+++ routes/GenCertificateRoutes.js
@@ -5,19 +5,26 @@ import Enrollment from "../Schema/Enrollment.js";
 const router = express.Router()
 
 router.post("/generateCertificate", errorHandling(async (req, res) => {
-    const { courseId, status, generateDate } = req.body
+    const { courseId, status, generateDate } = req.body;
 
-    if (!courseId) return res.status(400).json({ message: "please select course first" })
+    if (!courseId || !generateDate) return res.status(400).json({ message: "Please select * fields" });
 
-    const enrolledCourses = await Enrollment.find({ courseId, status: "Y" })
-    const candidateId = enrolledCourses.map(course => course.studentId)
-    if (candidateId.length === 0) return res.status(400).json({ message: "No student enrolled for this course" })
+    const enrolledCourses = await Enrollment.find({ courseId, status: "Y" });
+    const candidateId = enrolledCourses.map(course => course.studentId);
 
-    const generateCertificate = await GenCertificate.create({
-        courseId, status, candidateId, generateDate
-    })
-    res.json(generateCertificate)
-}))
+    if (candidateId.length === 0) {
+        return res.status(400).json({ message: "No student enrolled for this course" });
+    }
+
+    const updatedCertificate = await GenCertificate.findOneAndUpdate(
+        { courseId },
+        { $set: { candidateId, generateDate, status } },
+        { new: true, upsert: true }
+    );
+
+    res.json(updatedCertificate);
+}));
+
 
 router.get("/generatedCertificate", errorHandling(async (req, res) => {
     const generatedCertificates = await GenCertificate.find().populate("courseId").populate("candidateId")
